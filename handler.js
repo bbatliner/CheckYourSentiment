951c5801d0b470826abec28d1ad1eb39f43d163c
Brendan Batliner <brendan.batliner@gmail.com>
2017-04-15 15:34:27 -0500

    POST route for Watson Tone Analyzer

--- handler.js
+++ handler.js
@@ -1,4 +1,12 @@
-'use strict';
+'use strict'
+
+const watson = require('watson-developer-cloud')
+const toneAnalyzer = watson.tone_analyzer({
+  username: process.env.TONE_ANALYZER_USERNAME,
+  password: process.env.TONE_ANALYZER_PASSWORD,
+  version: 'v3',
+  version_date: '2016-05-19'
+})
 
 module.exports.hello = (event, context, callback) => {
   const response = {
@@ -6,11 +14,32 @@ module.exports.hello = (event, context, callback) => {
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
-    }),
-  };
+    })
+  }
 
-  callback(null, response);
+  callback(null, response)
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+}
+
+module.exports.analyze = (event, context, callback) => {
+  const response = {}
+  const body = JSON.parse(event.body)
+
+  if (typeof body.text !== 'string') {
+    callback(null, { statusCode: 400, body: JSON.stringify({ message: 'The "text" parameter is required.' }) })
+    return
+  }
+
+  toneAnalyzer.tone({ text: body.text }, (err, tone) => {
+    if (err) {
+      callback(null, { statusCode: 500, body: JSON.stringify({ message: err.message }) })
+      return
+    }
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(tone)
+    })
+  })
+}
