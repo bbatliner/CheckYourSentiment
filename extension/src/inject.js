cb595c2f68bdfcc56e1b5e74ecf6658b8cf1ebd8
Brendan Batliner <brendan.batliner@gmail.com>
2017-04-15 18:50:00 -0500

    CORS on analyze function and fetch for post content

--- extension/src/inject.js
+++ extension/src/inject.js
@@ -1,4 +1,46 @@
+'use strict'
+
+// This WeakMap maps DOM Nodes to `true`. It exists to tell us if we've seen this DOM node before,
+// without holding strong references and preventing DOM nodes that have been removed from the DOM
+// from being garbage collected.
+const inputs = new WeakMap()
 const composer = document.querySelector('#pagelet_composer')
 composer.addEventListener('click', () => {
-  console.log('Clicked on the post composer')
+  poll(() => composer.querySelector('[contenteditable=true]'))
+    .then(input => {
+      // Only do our processing if we haven't seen this contenteditable element before
+      if (inputs.get(input)) {
+        return
+      }
+      inputs.set(input, true)
+
+      input.addEventListener('input', () => {
+        console.log(input.textContent)
+      })
+
+      // Create an Analyze Post button
+      const analyzeButton = stringToDom('<button class="_1mf7 _4jy0 _4jy3 _4jy1 _51sy selected _42ft"><span class=""><em class="_4qba">Analyze Post</em></span></button>')
+      analyzeButton.addEventListener('click', () => {
+        // Analyze the sentiment of the post content, if it exists
+        const text = input.textContent
+        if (text.length === 0) {
+          return
+        }
+        fetch('https://lubnpw4d3b.execute-api.us-east-1.amazonaws.com/dev/analyze', {
+          method: 'post',
+          body: JSON.stringify({ text })
+        }).then(response => {
+          return response.json()
+        }).then(data => {
+          console.log(data)
+        }).catch(err => {
+          console.error(err)
+        })
+      })
+
+      // Add the Analyze Post button to the post box
+      const buttonContainer = document.querySelector('button[data-testid="react-composer-post-button"]').parentNode
+      buttonContainer.children[0].style.paddingLeft = '10px'
+      buttonContainer.insertBefore(analyzeButton, buttonContainer.children[0])
+    })
 })
--- serverless/handler.js
+++ serverless/handler.js
@@ -8,38 +8,38 @@ const toneAnalyzer = watson.tone_analyzer({
   version_date: '2016-05-19'
 })
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    })
+function getResponse (statusCode, body) {
+  return {
+    statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      'Access-Control-Allow-Credentials': true // Required for cookies, authorization headers with HTTPS 
+    },
+    body: JSON.stringify(body)
   }
-
-  callback(null, response)
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 }
 
 module.exports.analyze = (event, context, callback) => {
   const response = {}
-  const body = JSON.parse(event.body)
+
+  let body
+  try {
+    body = JSON.parse(event.body)
+  } catch (err) {
+    callback(null, getResponse(500, { message: 'Request body could not be parsed.' }))
+    return
+  }
 
   if (typeof body.text !== 'string') {
-    callback(null, { statusCode: 400, body: JSON.stringify({ message: 'The "text" parameter is required.' }) })
+    callback(null, getResponse(400, { message: 'The "text" parameter is required.' }))
     return
   }
 
   toneAnalyzer.tone({ text: body.text }, (err, tone) => {
     if (err) {
-      callback(null, { statusCode: 500, body: JSON.stringify({ message: err.message }) })
+      callback(null, getResponse(500, { message: err.message }))
       return
     }
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(tone)
-    })
+    callback(null, getResponse(200, tone))
   })
 }
